dfe2cd41d7a34e016f102f0dd5db962ab9c224be
AngeloMihaelle <angelomihaelle@gmail.com>
2025-07-02 02:27:50 -0600

    Added Documentation for the web server and updated the port number to 8081

--- main.go
+++ main.go
@@ -1,3 +1,5 @@
+// Package main implements a simple web server that serves static files
+// from the "./public" directory on port 8081.
 package main
 
 import (
@@ -6,15 +8,20 @@ import (
 )
 
 func main() {
+	// Create a file server handler to serve files from the "public" directory.
 	fs := http.FileServer(http.Dir("./public"))
 
+	// Register the file server handler to handle requests to the root URL path.
 	http.Handle("/", fs)
+
+	// Log that the server is starting.
 	log.Println("Starting server on :8081")
 
+	// Start the HTTP server on port 8081.
 	err := http.ListenAndServe(":8081", nil)
 
+	// If the server fails to start, log the error and exit.
 	if err != nil {
 		log.Fatal(err)
 	}
 }
-// comentario wuu
