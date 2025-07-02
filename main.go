f3826c7ac849deeacd8afbe346ad1bcefe979268
AngeloMihaelle <angelomihaelle@gmail.com>
2025-07-02 02:32:23 -0600

    Update main.go added variable port to 8081

--- main.go
+++ main.go
@@ -1,11 +1,15 @@
 // Package main implements a simple web server that serves static files
-// from the "./public" directory on port 8081.
+// from the "./public" directory on a configurable port.
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 )
+
+// port is the TCP port number the HTTP server listens on.
+var port = 8081
 
 func main() {
 	// Create a file server handler to serve files from the "public" directory.
@@ -15,10 +19,10 @@ func main() {
 	http.Handle("/", fs)
 
 	// Log that the server is starting.
-	log.Println("Starting server on :8081")
+	log.Printf("Starting server on :%d\n", port)
 
-	// Start the HTTP server on port 8081.
-	err := http.ListenAndServe(":8081", nil)
+	// Start the HTTP server on the specified port.
+	err := http.ListenAndServe(fmt.Sprintf(":%d", port), nil)
 
 	// If the server fails to start, log the error and exit.
 	if err != nil {
